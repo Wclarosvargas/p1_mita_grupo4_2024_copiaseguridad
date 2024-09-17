47df901b9b8e87680faf77a8556e2fadd2c5bd6a
Wclarosvargas <wclarosvargas@uade.edu.ar>
2024-09-17 20:33:29 -0300

    Se creo la copia de seguridad

---
+++ cursos.py
@@ -0,0 +1,194 @@
+from validaciones import validar_id_unico_clase, validar_horario, validar_id_profesor, validar_fecha
+
+"""
+cursos = [
+    [202,102,'15-09-2024','10:30-11:30'],
+    [205,12,'17-07-2024', '11:40-13:20']
+]
+"""
+
+# Lista de profesores
+profesores = [
+    [14, 'Carlos', 'Pérez'],
+    [25, 'Ana', 'García'],
+    [36, 'Luis', 'Martínez'],
+    [43, 'María', 'Lopez'],
+    [56, 'José', 'Hernández'],
+    [62, 'Laura', 'Gómez'],
+    [79, 'Miguel', 'Rodríguez'],
+    [80, 'Sofía', 'Fernández'],
+    [96, 'Jorge', 'Sánchez'],
+    [107, 'Isabel', 'Ramírez']
+]
+
+def crear_clase(matriz):
+    '''
+    Se encargara del ingreso de datos de la clase
+    '''
+
+    id_valido = 0
+    while id_valido == 0:
+        print("Ingrese el ID de la clase:")
+        id = int(input())
+        if validar_id_unico_clase(matriz,id):
+            id_valido = 1
+        else:
+            print("El ID de la clase ya existee, Por favor, ingrese un ID diferente.")
+
+    print("Ingrese el ID del profesor:")
+    id_profesor = int(input())         
+    if validar_id_profesor(profesores,id_profesor) == 0:
+        print("ID del profesor no válido. Por favor, ingrese un ID de profesor válido. ")
+        return
+
+    horario_valido = 0
+    while horario_valido == 0:
+        print("Ingrese el horario de la clase (por ejemplo, '09:00-10:00'):")
+        horario = input()
+        if validar_horario(horario) == 1:
+            horario_valido = 1
+        else:
+            print("Horario inválido.Por favor, ingrese un horario válido.")   
+
+    fecha_valida = 0
+    while fecha_valida == 0:
+        print("Ingrese la fecha de la clase (por ejemplo, '10-09-2024'):")
+        fecha = input()
+        if validar_fecha(fecha) == 1:
+            fecha_valida = 1
+        else:
+            print("Fecha inválida. Por favor, ingresar una fecha con formato 'DD-MM-YYYY'.")
+
+    nueva_clase = [id,id_profesor,fecha,horario]
+    print("Clase agregada con éxito.")
+    matriz.append(nueva_clase)
+
+
+def mostrar_curso(matriz):
+    '''
+    Pos: está funcion se encargara de mostrar la matriz de cursos
+    '''
+    curso = [[id,id_profesor,fecha[:13],horario[:13]] for id,id_profesor,fecha,horario in matriz]
+
+    #Impresión de encabezados
+    print(f"| {'ID':<5} | {'ID_profesor':<5} | {'Fecha':>13} | {'Horario':>20} |")  
+    print("-"*55)          
+
+    #Impresión de filas de datos
+    for i in curso:
+        print(f"| {i[0]:<5} | {i[1]:>8} | {i[2]:>17} | {i[3]:>20} |")
+
+#Definición actualizar matriz cursos
+#Modificar esta función para que modifique el id_curso, id del profesor, fecha, horario 
+def actualizar_curso(matriz):
+    '''
+    Actualiza los datos de la matríz curso mediante el ingreso de los datos
+    Se espera que sean modificados id_curso, id_profesor, fecha y horario
+    '''
+    print("Ingrese el ID del curso que desea actualizar:")
+    id_curso = int(input())
+
+    #Busca el curso por su ID
+    for curso in range(len(matriz)):
+        if matriz[curso][0] == id_curso:
+            print("Curso encontrado.")
+
+            
+
+            #Solicita el nuevo ID del profesor
+            flag = 0
+            while flag == 0:
+                print("Ingrese el nuevo ID del profesor:")
+                id_profesor = int(input())
+
+                if validar_id_profesor(profesores,id_profesor) == 1:
+                    flag = 1
+                else:
+                    print("ID del profesor no válido. Por favor, ingrese un ID de profesor válido.")
+            
+
+            #Solicita la nueva fecha
+            flag = 0
+            while flag == 0:
+                print("Ingrese la nueva fecha (formato DD-MM-YYYY):")
+                fecha = input()
+
+            #Valida la fecha ingresada
+                if validar_fecha(fecha) == 1:
+                    flag = 1
+                else:
+                    print("Fecha inválida. Por favor, ingresar una fecha con formato 'DD-MM-YYYY'.")
+                
+            
+            #Solicitar el nuevo horario
+            flag = 0
+            while flag == 0:
+                print("Ingrese el nuevo horario (formato HH:MM-HH:MM)")
+                horario = input()
+
+            #Validar la variable horario
+                if validar_horario(horario) == 1:
+                    flag = 1
+                else:
+                    print("Horario inválido. Por favor, ingrese un horario válido en formato 'HH:MM-HH:MM'.")
+
+            #Actualización de los datos
+            matriz[curso] = [id_curso, id_profesor, fecha, horario]
+            print("Los datos del curso fueron actualizados exitosamente.")
+            return 
+        
+    print("ID del curso no encontrado.")    
+
+#Se definio la función eliminar del CRUD
+def eliminar_curso(matriz):
+    '''
+    Tomara el id del curso y eliminara todos los elementos pertenecientes a ese id
+    '''            
+    print("Ingrese el ID del curso que desea eliminar:") 
+    id = int(input())       
+
+    #Busca el estudiante por ID
+    for curso in range(len(matriz)):
+        if matriz[curso][0] == id:
+            matriz.pop(curso) #La función pop eliminara los datos de la matríz que se encuentra en el ID ingresado
+            print("El curso fue eliminado con éxito")
+            return matriz
+    print("Curso no fue encontrado. Por favor ingrese nuevamanente el ID del curso a buscar")   
+    return matriz 
+
+
+"""
+
+def mostrar_menu():
+    '''
+    Muestra el menú de opciones del CRUD.
+    '''
+    print('Seleccionar una opción, (1)Crear, (2) leer,(3)actualizar, (4)Eliminar, (5)Salir : ')
+
+
+def main():
+    
+
+    flag = 0
+
+    while flag  == 0:
+        mostrar_menu()
+        accion = int(input().strip())
+
+        if accion == 1:
+            crear_clase(cursos)
+        elif accion == 2:
+            mostrar_curso(cursos)
+        elif accion == 3:
+            actualizar_curso(cursos)
+        elif accion == 4:
+            eliminar_curso(cursos)
+        elif accion == 5:
+            print('Saliendo del programa')
+            flag = 1
+        else:
+            print('Opción no válida, por favor ingrese nuevamente el dígito: ')  
+
+main()
+
+"""
---
+++ validaciones.py
@@ -0,0 +1,100 @@
+import re
+
+
+def validadr_id_unico(matriz,id):
+    """
+    Verifica si el ID del estudiante ya existe en la matriz.
+    """
+    
+    for estudiante in matriz:
+        if estudiante[0] == id:
+            return 0 # ID ya existe
+    return 1 #ID es único
+    
+def validar_promedio(promedio):
+    """
+    Verifica si el promedio está entre 1 y 10
+    Devuelve 1 si el promedio es válido, 0 si no lo es.
+    """
+    return 1 if 1 <= promedio <= 10 else 0
+
+
+#Validaciones de la matriz clase
+
+def validar_id_unico_clase(matriz,id):
+    '''
+    Verifica si el Id de la clase ya existe en la matriz clase
+    '''
+    for clase in matriz:
+        if clase[0] == id:
+            return 0 #ID ya existe
+    return 1 # Id es único
+
+
+def validar_horario(horario):
+    """
+    Verifica si el horario está en el formato 'HH:MM-HH:MM'.
+    """
+    horario_range = r"^([01]\d|2[0-3]):([0-5]\d)-([01]\d|2[0-3]):([0-5]\d)$"
+    if re.match(horario_range, horario):
+        return 1
+    return 0
+
+def validar_id_profesor(matriz,id):
+    '''
+    Verifica si el id del profesor existe
+    '''
+    for profe in matriz:
+        if profe[0] == id:
+            return 1
+    return 0
+
+def validar_fecha(fecha):
+    """
+    Verifica si la fecha está en el formato 'DD-MM-YYYY' mediante el uso de expresiones regulares.
+    """
+    # Expresión regular para verificar el formato de fecha
+    patron = r'^\d{2}-\d{2}-\d{4}$'
+    if re.match(patron, fecha):
+        # La fecha se encuentra en el formato correcto
+        return 1
+    else:
+        # La fecha no se encuentra en el formato correcto
+        return 0
+    
+
+#funciones abocados a validaciones para la matriz asistencias
+
+def validar_id_unico_asistencia(matriz_asistencia,id_asistencia):
+    '''
+    Verifica si el ID  de asistencia no se encuentra repetido
+    '''
+    for asistencia in matriz_asistencia:
+        if asistencia[0] == id_asistencia:
+            return 0 # ID ya existe 
+    return 1 # ID único
+
+def validar_id_curso(matriz_curso,id_curso):
+    '''
+    Verifica si el ID del curso existe en la matriz de cursos.
+    '''
+    for curso in matriz_curso:
+        if curso[1] == id_curso:
+            return 0
+    return 1
+
+def validar_id_estudiante(matriz_estudiante, id_estudiante):
+    '''
+    Verifica si el ID del estudiante existe en la matriz de estudiantes.
+    '''
+    for estudiante in matriz_estudiante:
+        if estudiante[2] == id_estudiante:
+            return 0
+    return 1
+
+def validar_estado(estado):
+    '''
+    Valida que el estado del estudiante sea 'Presente' o 'Ausente'.
+    '''
+    return estado in {'presente', 'ausente'}
+
